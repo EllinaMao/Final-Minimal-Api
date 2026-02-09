89e29cf0e19e2ce0e85941ec08aacb5b53a6b639
Olga <xotaoshi@gmail.com>
2026-02-09 18:05:11 +0200

    fds

--- LastProject/Program.cs
+++ LastProject/Program.cs
@@ -1,9 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
+var allServices = builder.Services;
+
+builder.Services.AddSingleton<IUserRepository, UserRepository>();
+
 var app = builder.Build();
 
 
 
 
-app.MapGet("/", () => "Hello World!");
+
+app.MapWhen(context => context.Request.Path.StartsWithSegments("/"), appBuilder =>
+{
+    appBuilder.Run(async context =>
+    {
+        var users = context.RequestServices.GetService<IUserRepository, UserRepository>();
+    });
+
+});
 
 app.Run();
